de9efcecfecc37cee2f7c22b5577651a581f4c53
adrikomorebi <muleroadriana@gmail.com>
2024-08-22 09:47:50 -0400

    Delete reset.js

--- reset.js
+++
@@ -1,7 +0,0 @@
-// Function to reset the stopwatch
-function resetStopwatch() {
-    const iframe = document.querySelector('iframe'); // Adjust the selector if needed
-    if (iframe) {
-        iframe.contentWindow.postMessage('resetStopwatch', '*');
-    }
-}
